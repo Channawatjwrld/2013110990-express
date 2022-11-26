192285d439a06ab91b36ae8dd21590ca52c0d48c
Channawatjwrld <channawatsirivit@gmail.com>
2022-11-26 11:23:23 +0700

    Userjs

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,20 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  //res.send('Hello World');
+  res.status(200).json({
+    fullame: 'Channawat',
+   })
 });
+router.get('/bio', function(req, res, next) {
+  //res.send('Hello World');
+  res.status(200).json({
+    fullame: 'Channawat',
+    nickname: 'Keng',
+    hobby: 'ROeveryday',
+    gitusername: 'Channawatjwrld'
+   })
+});
+
 
 module.exports = router;

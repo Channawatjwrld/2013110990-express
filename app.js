f2e5b2f334e6b4a11b79da67b3f7a0e84f7d5173
Channawatjwrld <channawatsirivit@gmail.com>
2022-12-24 10:07:47 +0700

    ShopControlling

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var companyRouter = require('./routes/company');
 const staffRouter = require('./routes/staff')
+const shopRouter = require('./routes/shop');
 
 var app = express();
 
@@ -23,5 +24,6 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/company', companyRouter)
 app.use('/staff', staffRouter)
+app.use('/shop', shopRouter)
 
 module.exports = app;

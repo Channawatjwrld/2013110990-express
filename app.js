058fb6695ea160b8de50f8360a5b5f3e5787852c
Channawatjwrld <channawatsirivit@gmail.com>
2023-01-07 11:24:01 +0700

    Workshop B

--- app.js
+++ app.js
@@ -17,7 +17,9 @@ const shopRouter = require('./routes/shop');
 var app = express();
 
 app.use(logger('dev'));
-app.use(express.json());
+app.use(express.json({
+    limit: '50mb'
+}));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
--- controllers/staffController.js
+++ controllers/staffController.js
@@ -1,10 +1,33 @@
+const fs = require('fs');
+const path = require('path');
+const uuidv4 = require('uuid');
+const { promisify } = require('util')
+const writeFileAsync = promisify(fs.writeFile)
+const config = require("../config/index")
+
 const Staff = require('../models/staff')
-exports.index = async (req, res, next) => {
+/*exports.index = async (req, res, next) => {
     const staff = await Staff.find().sort({_id: -1 })
     res.status(200).json({
       data: staff
     });
-};
+};*/
+exports.index = async (req, res, next) => {
+    const staff = await Staff.find()
+      .select("name photo")
+      .sort({ _id: -1 });
+  
+    const staffpWithPhotoDomain = staff.map((staff, index) => {
+      return {
+        name: staff.name,
+        photo: config.DOMAIN + staff.photo
+      };
+    });
+  
+    res.status(200).json({
+      data: staffpWithPhotoDomain,
+    });
+  };
 
 exports.show = async (req, res, next) => {
     try {const {id} = req.params
@@ -54,11 +77,12 @@ exports.destroy = async (req, res, next) => {
 
 exports.insert = async (req, res, next) => {
 
-    const{ name,salary } = req.body
+    const{ name,salary,photo } = req.body
     
     let staff = new Staff({
         name: name,
         salary: salary,
+        photo: await saveImageToDisk(photo)
     })
     await staff.save()
       res.status(200).json({
@@ -101,3 +125,42 @@ exports.insert = async (req, res, next) => {
           })
     }
   };
+
+  async function saveImageToDisk(baseImage) {
+    //หา path จริงของโปรเจค
+    const projectPath = path.resolve('./') ;
+    //โฟลเดอร์และ path ของการอัปโหลด
+    const uploadPath = `${projectPath}/public/images/`;
+  
+    //หานามสกุลไฟล์
+    const ext = baseImage.substring(baseImage.indexOf("/")+1, baseImage.indexOf(";base64"));
+  
+    //สุ่มชื่อไฟล์ใหม่ พร้อมนามสกุล
+    let filename = '';
+    if (ext === 'svg+xml') {
+        filename = `${uuidv4.v4()}.svg`;
+    } else {
+        filename = `${uuidv4.v4()}.${ext}`;
+    }
+  
+    //Extract base64 data ออกมา
+    let image = decodeBase64Image(baseImage);
+  
+    //เขียนไฟล์ไปไว้ที่ path
+    await writeFileAsync(uploadPath+filename, image.data, 'base64');
+    //return ชื่อไฟล์ใหม่ออกไป
+    return filename;
+  }
+  
+  function decodeBase64Image(base64Str) {
+    var matches = base64Str.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/);
+    var image = {};
+    if (!matches || matches.length !== 3) {
+        throw new Error('Invalid base64 string');
+    }
+  
+    image.type = matches[1];
+    image.data = matches[2];
+  
+    return image;
+  }
